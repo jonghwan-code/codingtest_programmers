235d2c238cf9c3b7bfc408e1713ce34e8a629ad8
Jonghwan <84922505+jonghwan-code@users.noreply.github.com>
2023-09-14 12:04:02 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -0,0 +1,11 @@
+def solution(s):
+    num = list(map(str, range(10)))
+    if len(s) == 4 or len(s) == 6:
+        for x in s:
+            if x not in num:
+                return False
+                break
+        else:
+            return True
+    else:
+        return False
