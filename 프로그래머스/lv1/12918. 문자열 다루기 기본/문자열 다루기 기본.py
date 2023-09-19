da2be08e70488d6010c0ce135484a9fb0b684dc9
Jonghwan <84922505+jonghwan-code@users.noreply.github.com>
2023-09-19 13:58:12 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,11 +1,12 @@
 def solution(s):
-    num = list(map(str, range(10)))
+    # num = list(map(str, range(10)))
     if len(s) == 4 or len(s) == 6:
-        for x in s:
-            if x not in num:
-                return False
-                break
-        else:
-            return True
+        # for x in s:
+        #     if x not in num:
+        #         return False
+        #         break
+        # else:
+        #     return True
+        return s.isdigit()
     else:
         return False
