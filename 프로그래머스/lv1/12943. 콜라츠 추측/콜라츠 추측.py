13f622b3da8d4700bd844e53853cfe16f3aada18
Jonghwan <84922505+jonghwan-code@users.noreply.github.com>
2023-09-19 11:11:39 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,15 +1,14 @@
+def collatz(x, count):
+    if count == 500:
+        return -1
+    if x == 1:
+        return count
+    if x % 2 == 1:
+        return collatz(x * 3 + 1, count + 1)
+    if x % 2 == 0:
+        return collatz(x / 2, count + 1)
+
 def solution(num):
-    def collatz(x, count):
-        if count == 500:
-            return -1
-        elif x == 1:        
-            return count
-        else:
-            if x % 2 == 1:
-                return collatz(x * 3 + 1, count + 1)
-            elif x % 2 == 0:
-                return collatz(x / 2, count + 1)
-    
     if num == 1:
         return 0
     else:
