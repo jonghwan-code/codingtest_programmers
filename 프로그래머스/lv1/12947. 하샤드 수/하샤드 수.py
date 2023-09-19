2ddcd3ee557e2753fc1650be184b181852607744
Jonghwan <84922505+jonghwan-code@users.noreply.github.com>
2023-09-19 11:16:48 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
@@ -1,6 +1,7 @@
 def solution(x):
     tmpSum = sum(list(map(int, (str(x)))))
-    if x % tmpSum:
-        return False
-    else:
-        return True
+    # if x % tmpSum:
+    #     return False
+    # else:
+    #     return True
+    return x % tmpSum == 0
