91acb4f4c57c25ca1082446fcdbacae0cdc5da66
Jonghwan <84922505+jonghwan-code@users.noreply.github.com>
2023-09-27 12:22:34 +0900

    [level 1] Title: 삼총사, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -1,13 +1,12 @@
 def solution(number):
     cb = [0] * 3
-    res = []
-    def DFS(L, S):
+    def DFS(L, S, res=[]):
         if L == 3:
             if sum(cb) == 0:
                 res.append(1)
         else:
             for i in range(S, len(number)):
                 cb[L] = number[i]
-                DFS(L + 1, i + 1)
-    DFS(0, 0)
-    return len(res)
+                DFS(L + 1, i + 1, res)
+        return len(res)
+    return DFS(0, 0)
