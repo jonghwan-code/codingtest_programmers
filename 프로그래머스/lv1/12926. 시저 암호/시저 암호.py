5fd957a9a4f7269a4852b218e8b255ff6034b7f0
Jonghwan <84922505+jonghwan-code@users.noreply.github.com>
2023-10-05 18:53:17 +0900

    [level 1] Title: 시저 암호, Time: 2.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
@@ -1,15 +1,12 @@
-# def check(c, number):
-#     if 97 <= ord(c) <= 122:
-#         return chr((ord(c) - ord('a') + number) % 26 + ord('a'))
-#     if 65 <= ord(c) <= 90:
-#         return chr((ord(c) - ord('A') + number) % 26 + ord('A'))
 def solution(s, n):
     res = ''
     for x in s:
         if x == ' ':
-            res += ' '
-        elif 97 <= ord(x) <= 122:
-            res += chr((ord(x) - ord('a') + n) % 26 + ord('a'))
+            res += x
         else:
-            res += chr((ord(x) - ord('A') + n) % 26 + ord('A'))
+            if 'A' <= x <= 'Z':
+                tmp = 'A' 
+            else:
+                tmp = 'a'
+            res += chr((ord(x) - ord(tmp) + n) % 26 + ord(tmp))
     return res
