4eb02190c522e749a02cae5732d60c55ee5fd64c
Jonghwan <84922505+jonghwan-code@users.noreply.github.com>
2023-10-13 00:21:46 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,21 +1,16 @@
-# def solution(s):
-#     ref = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-#     result = ''
-#     tmp = ''
-#     for c in s:
-#         if c.isdigit():
-#             result += c
-#         else:
-#             tmp += c
-#             if tmp in ref:
-#                 result += str(ref.index(tmp))
-#                 tmp = ''
-#             else:
-#                 continue
-#     return int(result) 
+from collections import deque
 def solution(s):
     ref = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    answer = s
-    for idx, value in enumerate(ref):
-        answer = answer.replace(value, str(idx))
-    return int(answer)
+    result = ''
+    stringChr = ''
+    s = deque(s)
+    while s:
+        char = s.popleft()
+        if char.isdigit():
+            result += char
+        else:
+            stringChr += char
+            if stringChr in ref:
+                result += str(ref.index(stringChr))
+                stringChr = ''
+    return int(result) 
