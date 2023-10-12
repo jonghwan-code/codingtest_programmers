3e99a29a989b155b39da32121807864c9709b61d
Jonghwan <84922505+jonghwan-code@users.noreply.github.com>
2023-10-12 16:25:20 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,24 @@
+def solution(n, arr1, arr2):
+    def computeBinaryNum(num):
+      binaryNum = bin(num)[2:]
+      return int(binaryNum)
+    
+    def makeFiveLength(binarySum):
+        if len(binarySum) < n:
+          return '0' * (n-len(binarySum)) + binarySum
+        return binarySum
+    
+    toBinaryNumA = list(map(computeBinaryNum, arr1))
+    toBinaryNumB = list(map(computeBinaryNum, arr2))
+
+    wallArr = [makeFiveLength(str(x + y)) for x, y in zip(toBinaryNumA, toBinaryNumB)]
+    res= []
+    for wall in wallArr:
+        number = wall
+        for code in wall:
+            if int(code) >= 1:
+                number = number.replace(code, '#')
+            else:
+                number = number.replace(code, ' ')
+        res.append(number)
+    return res  
