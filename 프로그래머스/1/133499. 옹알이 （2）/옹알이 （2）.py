6853ad574d9b39eada8e568e8913ba1481991a20
Jonghwan <84922505+jonghwan-code@users.noreply.github.com>
2023-10-16 16:27:40 +0900

    [level 1] Title: 옹알이 (2), Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -0,0 +1,11 @@
+def solution(babbling):
+    speakable = ["aya", "ye", "woo", "ma"]
+    count = 0
+    for word in babbling:
+        for s in speakable:
+            if s * 2 not in word:
+                word = word.replace(s, ' ')
+        
+        if word.strip() == '':
+            count += 1
+    return count
