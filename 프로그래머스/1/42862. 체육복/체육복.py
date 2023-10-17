1ad929c6b404bc6be7d800a24a997abbc64b0764
Jonghwan <84922505+jonghwan-code@users.noreply.github.com>
2023-10-17 17:09:20 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,20 @@
+def solution(n, lost, reserve):
+    stduent_gymsuit = [1]*n
+    for x in range(n):
+        if x + 1 in lost:
+            stduent_gymsuit[x] -= 1
+        if x + 1 in reserve:
+            stduent_gymsuit[x] += 1
+    # print('전', stduent_gymsuit)
+
+    for idx, val in enumerate(stduent_gymsuit):
+        if val == 0:
+            if 0 <= idx - 1 and stduent_gymsuit[idx - 1] > 1:
+                stduent_gymsuit[idx - 1] -= 1
+                stduent_gymsuit[idx] += 1
+            elif idx + 1 < n and stduent_gymsuit[idx + 1] > 1:
+                stduent_gymsuit[idx + 1] -= 1
+                stduent_gymsuit[idx] += 1
+    # print(stduent_gymsuit)
+
+    return len(list(filter(lambda x: x > 0, stduent_gymsuit)))
