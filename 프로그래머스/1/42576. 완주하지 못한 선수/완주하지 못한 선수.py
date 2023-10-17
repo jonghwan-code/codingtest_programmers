be2638eda6e9c6d0803b8e8f4d6dcbe0d989e898
Jonghwan <84922505+jonghwan-code@users.noreply.github.com>
2023-10-17 17:40:43 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 47.01 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,16 @@
+def solution(participant, completion):
+    dictPart = dict()
+    answer = ''
+    for part in participant:
+        if part in dictPart:
+            dictPart[part] += 1
+        else:
+            dictPart[part] = 1
+
+    for comPlayer in completion:
+        dictPart[comPlayer] -= 1
+
+    for player in dictPart:
+        if dictPart[player] > 0:
+            answer += player
+    return answer
