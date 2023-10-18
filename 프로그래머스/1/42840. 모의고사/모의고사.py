da12ef7caa9994b1d80a9261326fe9fd30e6f5c3
Jonghwan <84922505+jonghwan-code@users.noreply.github.com>
2023-10-18 17:47:13 +0900

    [level 1] Title: 모의고사, Time: 1.92 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -6,6 +6,8 @@ def solution(answers):
     oneP = test([1, 2, 3, 4, 5], answers).count(True)
     twoP = test([2, 1, 2, 3, 2, 4, 2, 5], answers).count(True)
     thiP = test([3, 3, 1, 1, 2, 2, 4, 4, 5, 5], answers).count(True)
-    student_grade = [(1,oneP), (2,twoP), (3,thiP)]
-    mx = max(map(lambda x: x[1], student_grade))
-    return list(map(lambda x: x[0], filter(lambda x: x[1] == mx, student_grade)))
+    student_grade = [(1, oneP), (2, twoP), (3, thiP)]
+    mx = max([p for s, p in student_grade])
+    # filtered = [(a, b) for a, b in student_grade if b == mx]
+    # return list(map(lambda x: x[0], filtered))
+    return [s for s, p in [(s, p) for s, p in student_grade if p == mx]]
